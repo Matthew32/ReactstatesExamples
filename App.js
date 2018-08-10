71b1ef62f8ec916fea202fa70144a793fe312409
Matthew <mmartinezmateo93@gmail.com>
2018-08-10 15:01:27 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+
+import Blink from './app/components/Blink.js';
+
+export default class BlinkApp extends Component {
+  render() {
+    return (
+      <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
+      </View>
+    );
+  }
+}
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => BlinkApp);
